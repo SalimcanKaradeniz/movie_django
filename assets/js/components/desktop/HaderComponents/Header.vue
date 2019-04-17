a801c4b1bcb18b2927ec0f6eb70b27932d3931fc
cenkay <cenkayaydin99@gmail.com>
2019-04-17 15:19:25 +0300

    Header tasarımı tamamlandı

---
+++ assets/js/components/desktop/HaderComponents/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <header class="header-section header-2">
+        <div class="wrap-sticky" style="height: 93px">
+            <nav class="navbar navbar-default bootsnav navbar-sticky on no-full">
+                <div class="container">
+                    <div class="attr-nav">
+                        <ul>
+                            <li class="search">
+                                <a>search</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="navbar-header">
+                        <a class="navbar-brand">
+                            <img src="http://movie.themepul.com/images/logo3.png" class="logo" alt="logo">
+                        </a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="navbar-menu">
+                        <ul class="nav navbar-nav navbar-left">
+                            <li>
+                                <a>event</a>
+                            </li>
+                            <li>
+                                <a>event</a>
+                            </li>
+                            <li>
+                                <a>event</a>
+                            </li>
+                            <li>
+                                <a>event</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
