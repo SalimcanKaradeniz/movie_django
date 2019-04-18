e544256b1be6faccd0583c78e5d3b226607d8654
oguzhan <oguzhankaradeniz6@gmail.com>
2019-04-18 12:11:04 +0300

    #1 closed

--- assets/js/mobile_router.js
+++ assets/js/mobile_router.js
@@ -1,9 +1,18 @@
 import Router from 'vue-router';
 import Vue from 'vue';
+// import ContentBar from './components/mobile/body/ContentBar.vue';
 
 Vue.use(Router);
 
-const routes = [];
+const routes = [
+    // {
+    //     // path: '/',
+    //     // component: ContentBar,
+    //     // meta: {
+    //     //     title: 'Anasayfa'
+    //     // }
+    // },
+];
 
 const router = new Router({
     routes
