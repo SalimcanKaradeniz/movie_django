bd027b755e8651f1a0f5f1de6df6c0a691c8d2fc
cenkay <cenkayaydin99@gmail.com>
2019-04-18 12:14:51 +0300

    banner tasarımı tamamlandı

---
+++ assets/js/components/desktop/BannerComponents/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="slider-section slider-2 carousel bs-slider fade control-round indicators-line" id="bootstrap-touch-slider">
+        <div class="slider-items carousel-inner">
+            <div class="item">
+                <div class="slider-img">
+                    <img src="http://movie.themepul.com/images/slider/home-3/1.jpg" alt="banner">
+                </div>
+                <div class="slider-content">
+                    <h1>Film Name</h1>
+                    <div class="button-layer">
+                        <a class="playLink">
+                            <span class="flat-button">
+                                <div class="button-icon"><img src="/static/images/DesktopImages/play.png" alt=""></div>
+                                <span class="button-text">Oynat</span>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="slider-description">
+                        <p>1500'lerden beri kullanılmakta olan standard Lorem Ipsum metinleri ilgilenenler için yeniden üretilmiştir. Çiçero tarafından yazılan 1.10.32 ve 1.10.33 bölümleri</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Banner"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- assets/js/components/desktop/HaderComponents/Header.vue
+++ assets/js/components/desktop/HaderComponents/Header.vue
@@ -3,8 +3,6 @@
         <div class="wrap-sticky" style="height: 93px">
             <nav class="navbar navbar-default bootsnav navbar-sticky on no-full">
                 <div class="container">
-                    <!--search-->
-                    <Search></Search>
                     <!--logo-->
                     <Logo></Logo>
                     <!--navbar-->
@@ -16,12 +14,11 @@
 </template>
 
 <script>
-    import Search from "./HeaderSearch.vue"
     import Logo from "./HeaderLogo.vue"
     import Navbar from "./HeaderNavbar.vue"
     export default {
         name: "Header",
-        components: {Search, Logo, Navbar}
+        components: {Logo, Navbar}
     }
 </script>
 
