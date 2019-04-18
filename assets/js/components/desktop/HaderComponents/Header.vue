92874423f27a0996754650e45ee7c16ac2b68c12
cenkay <cenkayaydin99@gmail.com>
2019-04-18 17:18:16 +0300

    yeni tasarım belirlendi ve header tamamlandı.

--- assets/js/components/desktop/HaderComponents/Header.vue
+++ assets/js/components/desktop/HaderComponents/Header.vue
@@ -1,9 +1,20 @@
 <template>
+    <div class="header">
+        <div class="header-main">
+            <!--logo-->
+            <Logo></Logo>
+            <!--navbar-->
+            <Navbar></Navbar>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Logo from "./Logo.vue"
+    import Navbar from "./HeaderNavbar.vue"
     export default {
         name: "Header",
+        components: {Logo, Navbar}
     }
 </script>
 
