22fe1e7a9a9df1f03da3752c80ff01640e6230ec
oguzhan <oguzhankaradeniz6@gmail.com>
2019-04-19 14:30:27 +0300

    Mobil anasayfa tasarımı tamanlandı

---
+++ assets/js/components/mobile/body/homepageMovie.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="spotlights"
+         style="touch-action: pan-y; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+        <div class="spotlight-item">
+            <a href="#">
+                <div class="image-component spotlight-item-thumbnail">
+                    <div class="image-component__placeholder horizontal-thumbnail-16x9">
+                        <img alt="Çarpışma"
+                             src="/static/js/components/mobile/mobil-Img/anasayfaImg/designimg.jpg">
+                    </div>
+                </div>
+                <h4 class="spotlight-item-name">Çarpışma</h4>
+                <div class="spotlight-item-description">Yeni - 18. Bölüm</div>
+                <span class="spotlight-play-button"></span>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "homepageMovie"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- assets/js/mobile_router.js
+++ assets/js/mobile_router.js
@@ -1,17 +1,17 @@
 import Router from 'vue-router';
 import Vue from 'vue';
-// import ContentBar from './components/mobile/body/ContentBar.vue';
+import contentBar from './components/mobile/body/contentBar.vue';
 
 Vue.use(Router);
 
 const routes = [
-    // {
-    //     // path: '/',
-    //     // component: ContentBar,
-    //     // meta: {
-    //     //     title: 'Anasayfa'
-    //     // }
-    // },
+    {
+        path: '/',
+        component: contentBar,
+        meta: {
+            title: 'Anasayfa'
+        }
+    },
 ];
 
 const router = new Router({
